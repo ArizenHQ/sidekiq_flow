8a5b9ddf444bd6d19e6ec876f8e28c5ed81067ca
piasekhgw <wojciech.piasecki@netguru.pl>
2018-10-04 15:05:27 +0200

    fix json data attr

--- lib/sidekiq_flow/front/serializers/workflow_serializer.rb
+++ lib/sidekiq_flow/front/serializers/workflow_serializer.rb
@@ -1,9 +1,13 @@
-class WorkflowSerializer < SimpleDelegator
-  def to_h
-    __getobj__.to_h.merge(klass: klass, tasks: tasks.map { |t| t.to_h.merge(klass: t.klass, name: t.to_s) })
-  end
+module SidekiqFlow
+  module Front
+    class WorkflowSerializer < SimpleDelegator
+      def to_h
+        __getobj__.to_h.merge(klass: klass, tasks: tasks.map { |t| t.to_h.merge(klass: t.klass, name: t.to_s) })
+      end
 
-  def to_json
-    to_h.to_json
+      def to_json
+        to_h.to_json
+      end
+    end
   end
 end
