b8d7e955f483dd0e9eaebf2d02410162726838a4
piasekhgw <wojciech.piasecki@netguru.pl>
2018-11-16 16:53:39 +0100

    extend front serializer

--- lib/sidekiq_flow/front/serializers/workflow_serializer.rb
+++ lib/sidekiq_flow/front/serializers/workflow_serializer.rb
@@ -2,11 +2,27 @@ module SidekiqFlow
   module Front
     class WorkflowSerializer < SimpleDelegator
       def to_h
-        __getobj__.to_h.merge(klass: klass, tasks: tasks.map { |t| t.to_h.merge(klass: t.klass, name: t.to_s) })
+        __getobj__.to_h.merge(klass: klass, tasks: tasks.map { |t| task_hash(t) })
       end
 
       def to_json
         to_h.to_json
+      end
+
+      private
+
+      def task_hash(task)
+        task.to_h.merge(
+          klass: task.klass,
+          name: task.to_s,
+          start_date: formatted_date(task.start_date),
+          end_date: formatted_date(task.end_date)
+        )
+      end
+
+      def formatted_date(timestamp)
+        return if timestamp.nil?
+        Time.at(timestamp)
       end
     end
   end
